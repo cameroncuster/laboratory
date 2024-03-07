422fc41500358e0fe35682d15bc2a779cdd13239
cameron custer <custer.cameron@gmail.com>
2024-03-07 16:19:10 -0500

    add template & debug

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debug.hpp"
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	return 0;
+}
+
