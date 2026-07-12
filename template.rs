6f9e711871fd11e6d0a76448ae76e858e7ee3ea5
Cameron Custer <custer.cameron@gmail.com>
2026-07-12 14:05:31 -0700

    template.rs: slurp stdin by default, add input_interactive! and std prelude
    Match proconio's architecture: input! reads all of stdin once and hands
    out tokens from a single split_ascii_whitespace iterator (measured 20ms
    for 1e6 tokens vs 66ms for the lazy reader); input_interactive! keeps
    the lazy line-by-line path for interactive problems.
    
    Also add a use-everything prelude (std::collections::*, cmp utilities)
    as the bits/stdc++.h equivalent.

--- template.rs
+++ template.rs
@@ -1,7 +1,11 @@
 use std::io::{self, prelude::*};
+#[allow(unused_imports)]
+use std::cmp::{max, min, Ordering, Reverse};
+#[allow(unused_imports)]
+use std::collections::*;
 
-// proconio-style input! Tokens are read lazily line by line, so it also
-// works for interactive problems (unlike proconio's read-everything input!).
+// proconio-style input! Slurps all of stdin on first use (fastest); use
+// input_interactive! for interactive problems (reads lazily line by line).
 //
 //   input! {
 //       n: usize,
@@ -14,35 +18,60 @@ use std::io::{self, prelude::*};
 //   }
 #[allow(unused_macros)]
 macro_rules! input {
-    ($(,)?) => {};
-    (, $($rest:tt)*) => { input!($($rest)*); };
-    (mut $var:ident: $t:tt $($rest:tt)*) => {
-        let mut $var = read_value!($t);
-        input!($($rest)*);
+    ($($rest:tt)*) => { input_inner!(read_token, $($rest)*); };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_interactive {
+    ($($rest:tt)*) => { input_inner!(read_token_line, $($rest)*); };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($rd:ident $(,)?) => {};
+    ($rd:ident, mut $var:ident: $t:tt $($rest:tt)*) => {
+        let mut $var = read_value!($rd, $t);
+        input_inner!($rd $($rest)*);
     };
-    ($var:ident: $t:tt $($rest:tt)*) => {
-        let $var = read_value!($t);
-        input!($($rest)*);
+    ($rd:ident, $var:ident: $t:tt $($rest:tt)*) => {
+        let $var = read_value!($rd, $t);
+        input_inner!($rd $($rest)*);
     };
 }
 
 #[allow(unused_macros)]
 macro_rules! read_value {
-    (($($t:tt),*)) => { ($(read_value!($t),)*) };
-    ([$t:tt; $len:expr]) => { (0..$len).map(|_| read_value!($t)).collect::<Vec<_>>() };
-    ([$t:tt]) => {{
-        let len = read_value!(usize);
-        (0..len).map(|_| read_value!($t)).collect::<Vec<_>>()
+    ($rd:ident, ($($t:tt),*)) => { ($(read_value!($rd, $t),)*) };
+    ($rd:ident, [$t:tt; $len:expr]) => { (0..$len).map(|_| read_value!($rd, $t)).collect::<Vec<_>>() };
+    ($rd:ident, [$t:tt]) => {{
+        let len = read_value!($rd, usize);
+        (0..len).map(|_| read_value!($rd, $t)).collect::<Vec<_>>()
     }};
-    (Chars) => { read_token().chars().collect::<Vec<char>>() };
-    (Bytes) => { read_token().bytes().collect::<Vec<u8>>() };
-    (Usize1) => { read_value!(usize) - 1 };
-    (Isize1) => { read_value!(isize) - 1 };
-    ($t:ty) => { read_token().parse::<$t>().expect("parse failed") };
+    ($rd:ident, Chars) => { $rd().chars().collect::<Vec<char>>() };
+    ($rd:ident, Bytes) => { $rd().bytes().collect::<Vec<u8>>() };
+    ($rd:ident, Usize1) => { read_value!($rd, usize) - 1 };
+    ($rd:ident, Isize1) => { read_value!($rd, isize) - 1 };
+    ($rd:ident, $t:ty) => { $rd().parse::<$t>().expect("parse failed") };
 }
 
 #[allow(dead_code)]
 fn read_token() -> &'static str {
+    use std::sync::Mutex;
+    static TOKENS: Mutex<Option<std::str::SplitAsciiWhitespace<'static>>> = Mutex::new(None);
+    TOKENS
+        .lock()
+        .unwrap()
+        .get_or_insert_with(|| {
+            let mut buf = String::new();
+            io::stdin().read_to_string(&mut buf).expect("read failed");
+            Box::leak(buf.into_boxed_str()).split_ascii_whitespace()
+        })
+        .next()
+        .expect("input exhausted")
+}
+
+#[allow(dead_code)]
+fn read_token_line() -> &'static str {
     use std::sync::Mutex;
     static TOKENS: Mutex<Vec<&'static str>> = Mutex::new(Vec::new());
     let mut tokens = TOKENS.lock().unwrap();
